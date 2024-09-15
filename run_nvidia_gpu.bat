a45deb9d5b8f392ebd43dc038dbb935ea8f21b2a
lilly <lilly1987@naver.com>
2024-09-16 08:12:24 +0900

    1

--- run_nvidia_gpu.bat
+++ run_nvidia_gpu.bat
@@ -1,6 +1,6 @@
 rem Set vram state to: NORMAL_VRAM
-rem --cache-lru 64 --highvram
+rem  --highvram --cache-classic 
 :top
-..\python_embeded\python.exe -s ..\ComfyUI\main.py --cache-classic 
+..\python_embeded\python.exe -s ..\ComfyUI\main.py --cache-lru 128
 pause
 goto top
